36f450200bfd8ddcebe4115f56d560d2f1364684
souldough <yipingh@yeah.net>
2017-08-14 10:25:18 +0800

    add camera rolling

--- level1.js
+++ level1.js
@@ -2,9 +2,9 @@ const Level1 = {
     preload() {
 
     },
-    create() { 
+    create() {
         this.text_padding = 10
-        
+
         this.line_index = 0
 
         this.wordIndex = 0
@@ -13,15 +13,69 @@ const Level1 = {
 
         util.setScale(this)
 
-        
-        this.bg = game.add.image(game.world.centerX, game.world.centerY, 'level1_dialog_bg')
-        this.bg.scale.setTo(0.8)
+        this.cameraRoll = false
+        this.boundsW = game.cache._cache.image.level1_dialog_bg.frame.width
+        this.boundsH = game.cache._cache.image.level1_dialog_bg.frame.height
+        // game.world.setBounds(0, 0, this.boundsW, this.boundsH)
+        this.bg = game.add.sprite(700, 500, 'level1_dialog_bg')
+        util.centerGameObjects([this.bg])
+        let x_tween = game.add.tween(this.bg)
+        let y_tween = game.add.tween(this.bg)
+        let scale_tween = game.add.tween(this.bg.scale)
+        x_tween.to({x: game.world.centerX - 80}, 1000, Phaser.Easing.Default, true, 1000, 0, false)
+        x_tween.onComplete.addOnce(() => {
+            y_tween.to({y: game.world.centerY - 180}, 1000, Phaser.Easing.Default, true, 1000, 0, false)
+            y_tween.onComplete.addOnce(() => {
+                let position_tween = game.add.tween(this.bg)
+                position_tween.to({y: this.bg.y - 140}, 1000, Phaser.Easing.Default, true, 1000, 0, false)
+                scale_tween.to({ x: 1.5, y: 1.5 }, 1000, Phaser.Easing.Default, true, 1000, 0, false)
+            })
+        })
+        scale_tween.onComplete.addOnce(() => game.time.events.add(1000, this.startDialog, this), this)
+        // let timer = game.time.events.add(500, this.rollCamera, this)
 
+
+
+
+    },
+
+    update() {
+
+        // if (cursors.up.isDown) {
+        //     game.camera.y -= 4;
+        // }
+        // else if (cursors.down.isDown) {
+        //     game.camera.y += 4;
+        // }
+
+        // if (cursors.left.isDown) {
+        //     game.camera.x -= 4;
+        // }
+        // else if (cursors.right.isDown) {
+        //     game.camera.x += 4;
+        // }
+        // if (this.cameraRoll) {
+        //     if (game.camera.x < this.boundsW) {
+        //         game.camera.x += 10
+        //     }else if (game.camera.y < this.boundsH) {
+        //         game.camera.y += 10
+        //     } else {
+        //         this.cameraRoll = false
+        //     }
+
+        // }
+
+    },
+    render() {
+        game.debug.geom(this.prompt, 'rgba(255,0,0,0.5)')
+        game.debug.cameraInfo(game.camera, 0, 0)
+    },
+    startDialog() {
         this.dialog = game.cache.getJSON('dialog', true).level1
 
 
         let _w = game.cache._cache.image.dialog_box.frame.width
-    
+
         let _h = game.cache._cache.image.dialog_box.frame.height
         // this.dialogBox = game.add.image((game.world._width - _w) / 2, (game.world._height - _h) / 2, 'dialog_box')
         //对话框对象
@@ -33,13 +87,13 @@ const Level1 = {
         this.current = {}
         // 当前对话文本
         this.currentContent = ''
-        var style = { 
-            font: "28px custom", fill: "#fff", 
+        var style = {
+            font: "28px custom", fill: "#fff",
             boundsAlignH: "left",
-            boundsAlignV: "top", 
+            boundsAlignV: "top",
             align: 'left',
         };
-        var nameStyle = Object.assign({}, style, {font: "32px custom", fill: "#bd865d"})
+        var nameStyle = Object.assign({}, style, { font: "32px custom", fill: "#bd865d" })
         //当前对话文字对象
         this.text = game.add.text(0, 0, '', style);
         this.text.lineSpacing = 5
@@ -51,40 +105,26 @@ const Level1 = {
             this.dialogBox.height - 6 * this.text_padding
         )
 
-        // 当前对话人物名
-        this.currentNameText = ''
         // 当前人物名字对象
-        this.currentName = game.add.text(this.dialogBox.x + this.currentFacial.width + this.text_padding, this.dialogBox.y + 30, this.currentNameText, nameStyle)
-
-        //对话JSON
-
-        // this.currentContent = this.makeLine(this.currentContent)
-        // this.nextLine(this.currentContent)
+        this.currentName = game.add.text(this.dialogBox.x + this.currentFacial.width + this.text_padding, this.dialogBox.y + 30, '', nameStyle)
 
         //提示符对象
         this.prompt = game.add.image(this.dialogBox.x + this.dialogBox.width - 80, this.dialogBox.y + this.dialogBox.height - 42, 'dialog_prompt')
         this.prompt.alpha = 0
 
+        // //摄像机全景
+        // this.rollCamera()
+
         //载入对话信息
         this.nextDialog()
 
-        // this.facial = game.add.sprite(this.dialogBox.x + 10, this.dialogBox.y - 13, 'hero_facial')
-        util.centerGameObjects([this.bg])
+        
 
-        // this.currentContent = this.dialog.level1[0].text
-
-        // this.currentName = game.add.text(this.dialogBox.x + this.currentFacial.width + this.text_padding, this.dialogBox.y + 30, this.currentNameText, nameStyle)
+        // cursors = game.input.keyboard.createCursorKeys();
 
     },
 
-    update() {
-
-
-    },
-    render() {
-           game.debug.geom(this.prompt, 'rgba(255,0,0,0.5)')
-    },
-    nextDialog () {
+    nextDialog() {
         this.line_index = 0
         this.loadDialogText()
         this.text.text = ''
@@ -95,7 +135,6 @@ const Level1 = {
         if (this.current.y) {
             this.currentFacial.y = this.currentFacial.y + this.current.y
         }
-        this.currentName.text = this.currentNameText
         this.currentContent = this.makeLine(this.currentContent)
         this.nextLine()
     },
@@ -103,12 +142,12 @@ const Level1 = {
     makeLine(str) {
         let _str = str.split('\n')
         _str = _str.map((_p) => {
-            return  _p.split('')
+            return _p.split('')
         })
         for (let i = 0, length = _str.length; i < length; i++) {
             let p = _str[i]
             let num_space = 0
-            for (let j = 0, _len = p.length; j < _len ;j++) {
+            for (let j = 0, _len = p.length; j < _len; j++) {
                 if (j && j % game.global.LINE_WORDS == 0) {
                     p.splice(j + num_space, 0, '\n')
                     num_space++
@@ -117,7 +156,7 @@ const Level1 = {
         }
         let num_space = 0
         _str.forEach((p, i) => {
-            if (!i) 
+            if (!i)
                 return
             _str.splice(i + num_space, 0, '\n')
             num_space++
@@ -148,13 +187,13 @@ const Level1 = {
     },
     promptTween() {
         this.prompt.alpha = 1
-        this.tween_prompt = game.add.tween(this.prompt).to({y: this.prompt.y + 2}, 100, Phaser.Easing.Bounce.InOut, false, 100, 3, true).start()
+        this.tween_prompt = game.add.tween(this.prompt).to({ y: this.prompt.y + 2 }, 100, Phaser.Easing.Bounce.InOut, false, 100, 3, true).start()
         this.tween_prompt.onComplete.addOnce(this.promptTween, this)
     },
-    onTap (pointer) {
+    onTap(pointer) {
         if (this.currentContent.length != this.line_index) {
             this.currentContent.splice(0, this.line_index)
-            this.line_index = 0 
+            this.line_index = 0
             this.text.text = ''
             this.prompt.alpha = 0
             this.nextLine()
@@ -166,9 +205,15 @@ const Level1 = {
         game.tweens.remove(this.tween_prompt)
         this.prompt.alpha = 0
     },
-    loadDialogText () {
+    loadDialogText() {
         this.current = this.dialog.shift()
-        this.currentNameText = this.current.name
+        this.currentName.text = this.current.name
         this.currentContent = this.current.text
+    },
+    rollCamera() {
+        this.cameraRoll = true
+        // this.bg.scale.setTo(2)
+        // game.camera.scale.setTo(2)
+
     }
 }
